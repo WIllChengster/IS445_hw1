5ca848cb7ecc73a100e6358c55cba7412e209ed0
burninggun <willchengsters@gmail.com>
2020-01-28 20:08:22 -0800

    added javascript

---
+++ main.js
@@ -0,0 +1,22 @@
+const cat_arr = [
+    "./imgs/cat1.jpeg",
+    "./imgs/cat2.jpeg",
+    "./imgs/cat3.jpg",
+];
+
+
+const btn = document.getElementById('next-button');
+const img = document.querySelector('img');
+
+let counter = 0;
+
+btn.addEventListener('click', () => {
+    if(counter === cat_arr.length - 1){
+        counter = 0
+    }
+
+    img.src = cat_arr[counter]
+    counter ++
+
+
+})
